16f4a5bc55ea71bb6118f96ac490829cadd410d4
alfix <alfinewbie9@gmail.com>
2026-01-05 10:25:03 +0700

    menambah kode akses

--- login.js
+++ login.js
@@ -1,4 +1,15 @@
-const KODE_VALID = ["ROAS-123", "ROAS-456"];
+const KODE_VALID = [
+     "GA-9XQ7-LM82-ZP",
+     "RO4S-A7F9-KD2X",
+     "GADS-8PZ3-QM91",
+     "RXA9-27QF-LZ8K",
+     "GRW-FA92-ZXQ7",
+     "ADS-9KQ2-PX7M",
+     "GROW-7ZQ9-AF2X",
+     "RAOS-X29F-Q7ZK",
+     "GX-8FQ7-29ZA",
+     "GADS-Q9Z7-X2FA"
+];
 
 const inputKode = document.getElementById("kode");
 if (inputKode) inputKode.focus();
